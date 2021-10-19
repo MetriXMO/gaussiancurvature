4570d4d62500c4b97c086ffcef9156e353447244
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2021-10-19 23:24:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-import { AttentionSeeker } from 'react-awesome-reveal';
+
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, About, Skills, Experience, Contact, Footer } from './components'
 import { useState } from "react";
@@ -22,7 +22,7 @@ function App() {
       <div className="App" data-theme={darkMode ? "dark" : "light"}>
         <Route path="/">
           <Navbar expand="lg" className="navbar" data-theme={darkMode ? "dark" : "light"}>
-            <Navbar.Brand href="#home" className="navname">MMO</Navbar.Brand>
+            <Navbar.Brand href="#home" className="navname"><img src="/static/media/hello-light.5201f2ba.png" width="120"></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
