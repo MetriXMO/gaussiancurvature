f2ac95dbff107ac3da2e762322b78ae136321f60
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2021-10-22 02:44:59 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import { Slide } from "react-awesome-reveal";
-import skillsLight from '../media/skills-light.svg';
-import skillsDark from '../media/skills-dark.svg';
 import Container from 'react-bootstrap/esm/Container'
 import Row from 'react-bootstrap/esm/Row'
 import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/esm/Button';
 import Card from 'react-bootstrap/esm/Card';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCss3Alt, faDocker, faHtml5, faJs, faReact } from '@fortawesome/free-brands-svg-icons';
+
 
 const Skills = ({ darkMode }) => {
     return (
