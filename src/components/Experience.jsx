02afd305d4ac433cf52b3d32aa7a5d62ed7d1969
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2021-10-22 02:46:40 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { useState, React } from 'react'
+import { React } from 'react'
 import { Zoom, Fade } from 'react-awesome-reveal'
 import tcd from '../media/TCD.png'
 import Container from 'react-bootstrap/esm/Container'
