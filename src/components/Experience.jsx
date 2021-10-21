730f627341a63ecb14940893f0216193c9fd47a7
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2021-10-22 02:51:13 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,6 @@
 import { React } from 'react'
 import { Zoom, Fade } from 'react-awesome-reveal'
-import tcd from '../src/hello-light.png'
+import tcd from '../media/hello-light.png'
 import Container from 'react-bootstrap/esm/Container'
 import Button from 'react-bootstrap/esm/Button'
 import Row from 'react-bootstrap/esm/Row'
