7974a918c1cc709ff800d43824d9c9db8660e1d9
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2021-10-21 10:23:19 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { AttentionSeeker } from 'react-awesome-reveal';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import { Container } from 'react-bootstrap';
 import Card from 'react-bootstrap/esm/Card';
 
 const Home = ({ darkMode }) => {
