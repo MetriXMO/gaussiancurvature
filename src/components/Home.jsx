4b88920e8c48586a75e89b1a37819610acc245c2
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2021-10-21 10:21:29 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { AttentionSeeker } from 'react-awesome-reveal';
-import helloLight from '../media/hello-light.png';
-import helloDark from '../media/hello-dark.png';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { Container } from 'react-bootstrap';
