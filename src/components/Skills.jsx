5bb51237b72f0edbe38658ff345e30729e5f3302
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2021-10-25 17:29:52 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -42,9 +42,9 @@ MetriX Mathematical Olympiad held on 2020 Day 2 paper</Card.Text>
                                     <Card.Title>MMO 2020 Samples</Card.Title>
                                     <Card.Text>In progress
 <hr/>
-                                      <Button size="large" variant="contained" color="primary" href="https://aritra-12.github.io/pdfs/MMO%202020%20DAY%201.pdf">
+                                      <button type="button" href="https://aritra-12.github.io/pdfs/MMO%202020%20DAY%201.pdf">
                                     View
-                                </Button></Card.Text>
+                                </button></Card.Text>
                                 </Card.Body>
                             </Card>
                         </Col>
