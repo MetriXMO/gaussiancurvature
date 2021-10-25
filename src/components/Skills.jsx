13147c62b561af4b4dfa44a1172941ca499cf442
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2021-10-25 18:17:43 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Slide } from "react-awesome-reveal";
 import helloLight from '../components/cooool.png';
-import helloDark from '../components/cooool.png';
+import helloDark from '../components/cooool.png';import helloLight1 from '../components/cool.png';
+import helloDark1 from '../components/cool.png';
 import Container from 'react-bootstrap/esm/Container'
 import Button from 'react-bootstrap/esm/Button'
 import Row from 'react-bootstrap/esm/Row'
@@ -23,10 +24,8 @@ const Skills = ({ darkMode }) => {
                                 <Card.Body>
                                     <Card.Title align="center"><img src={darkMode ? helloDark : helloLight} alt="hello" className="hello-img" width="500" /></Card.Title>
                                     <Card.Text><h4><b>MMO 2020 Day 1</b></h4>
-                                                      <hr/>
-MetriX Mathematical Olympiad held on 2020 Day 1 paper
-  <br/>
-<a href="https://aritra-12.github.io/pdfs/MMO%202020%20DAY%201.pdf" target="_blank" rel="noreferrer noopener">
+                                                      <hr/>MetriX Mathematical Olympiad held on 2020 Day 1 paper
+  <a href="https://aritra-12.github.io/pdfs/MMO%202020%20DAY%201.pdf" target="_blank" rel="noreferrer noopener">
                                             <Button variant={darkMode ? "outline-light" : "outline-dark"} className="sbtn">View</Button>
                                         </a>&nbsp;</Card.Text>
                                 </Card.Body>
@@ -36,10 +35,8 @@ MetriX Mathematical Olympiad held on 2020 Day 1 paper
                                 <Card.Body >
                                    <Card.Title align="center"><img src={darkMode ? helloDark : helloLight} alt="hello" className="hello-img" width="500"/></Card.Title>
                                    <Card.Text><h4><b>MMO 2020 Day 2</b></h4>
-                                                
-MetriX Mathematical Olympiad held on 2020 Day 2 paper  
-    <br/>
-<a href="https://aritra-12.github.io/pdfs/MMO%202020%20DAY%202.pdf" target="_blank" rel="noreferrer noopener">
+                                                MetriX Mathematical Olympiad held on 2020 Day 2 paper  
+  <a href="https://aritra-12.github.io/pdfs/MMO%202020%20DAY%202.pdf" target="_blank" rel="noreferrer noopener">
                                             <Button variant={darkMode ? "outline-light" : "outline-dark"} className="sbtn">View</Button>
                                         </a>&nbsp;</Card.Text>
                                 </Card.Body>
