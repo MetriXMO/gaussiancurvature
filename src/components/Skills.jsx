50b611251b7ea1b63e59c8eb2815dfc28837ee13
Aritra-12 <67796196+Aritra-12@users.noreply.github.com>
2021-10-26 23:54:22 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,20 +13,6 @@ import Alert from 'react-bootstrap/Alert';
 function AlertDismissibleExample() {
   const [show, setShow] = useState(true);
 
-  if (show) {
-    return (
-      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-        <p>
-          Change this and that and try again. Duis mollis, est non commodo
-          luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-          Cras mattis consectetur purus sit amet fermentum.
-        </p>
-      </Alert>
-    );
-  }
-  return <Button onClick={() => setShow(true)}>Show Alert</Button>;
-}
 
 
 const Skills = ({ darkMode }) => {
@@ -65,12 +51,27 @@ const Skills = ({ darkMode }) => {
                              </Card></Col>  
                     </Row>
 
-render(<AlertDismissibleExample />);
+
                     <br></br>
                     <hr></hr>
                 </Container>
             </Slide>
         </div>
+    if (show) {
+    return (
+      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+        <p>
+          Change this and that and try again. Duis mollis, est non commodo
+          luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+          Cras mattis consectetur purus sit amet fermentum.
+        </p>
+      </Alert>
+    );
+  }
+  return <Button onClick={() => setShow(true)}>Show Alert</Button>;
+}
+<AlertDismissibleExample />
     )
 }
 
