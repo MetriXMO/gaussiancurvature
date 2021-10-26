ff036e3858244e8a34bef1930a83019cd14c82ef
Aritra-12 <67796196+Aritra-12@users.noreply.github.com>
2021-10-26 17:28:44 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -18,51 +18,9 @@ const Experience = ({ darkMode }) => {
             <Fade>
                 <Container>
                     <br></br>
-                    <h2 className="lead" align="center"><b>- Papers-</b></h2>
+                    <h2 className="lead" align="center"><b>Sponsors</b></h2>
                     <br></br>
-                    <Zoom>
-                        <Row>
-                            <Col lg={4} sm={12}>
-                                <Card align="center" className="exp" data-theme={darkMode ? "dark" : "light"}>
-                                    <br></br>
-                                    <center>
-                                        <div class="flip-card">
-                                            <div class="flip-card-inner">
-                                                <div className="flip-card-front">
-                                                    <Card.Img variant="top" src={tcd} />
-                                                    <Card.Body>
-                                                        <Card.Title align="center">In pr0gress</Card.Title>
-                                                        <Card.Text>
-                                                            <p align="center">In progess, ENT don't spam okay</p>
-                                                        </Card.Text>
-                                                    </Card.Body>
-                                                </div>
-                                                <div className="flip-card-back">
-                                                    <p>o
-                                                        <br></br>in
-                                                        <br></br>progress
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </center>
-                                    <br></br>
-                                    <div>
-                                        <a href="https://metrixmath-olympiad.vercel.app/" target="_blank" rel="noreferrer noopener">
-                                            <Button variant={darkMode ? "outline-light" : "outline-dark"} className="sbtn">Download</Button>
-                                        </a>&nbsp;
-
-                                        <a href="https://metrixmath-olympiad.vercel.app/" target="_blank" rel="noreferrer noopener">
-                                            <Button variant="dark" className="sbtn">Read</Button>
-                                        </a>
-                                    </div>
-                                    <br></br>
-                                </Card>
-                            </Col>
-                          
-                        </Row>
-                    </Zoom>
-                </Container>
+                  </Container>
             </Fade>
             <br></br>
             <hr></hr>
