af01cbd2d58d1beb14f8d28985485d4063b88ad8
Aritra-12 <67796196+Aritra-12@users.noreply.github.com>
2021-10-26 17:33:15 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,8 @@
 import { React } from 'react'
 import { Zoom, Fade } from 'react-awesome-reveal'
-import tcd from '../media/hello-light.png'
 import Container from 'react-bootstrap/esm/Container'
-import Button from 'react-bootstrap/esm/Button'
-import Row from 'react-bootstrap/esm/Row'
-import Col from 'react-bootstrap/esm/Col'
-import Card from 'react-bootstrap/esm/Card'
+
+
 
 
 const Experience = ({ darkMode }) => {
