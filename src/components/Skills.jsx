414662a171e569d3f0bda9f10adaa3987625ce4c
Aritra-12 <67796196+Aritra-12@users.noreply.github.com>
2021-10-27 11:05:19 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -18,16 +18,14 @@ import Alert from 'react-bootstrap/Alert';
   if (show) {
     return (
       <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+        <Alert.Heading>Announcement</Alert.Heading>
         <p>
-          Change this and that and try again. Duis mollis, est non commodo
-          luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-          Cras mattis consectetur purus sit amet fermentum.
+          MMO Registartion is going to start very soon, time to get thrilled and excited !
         </p>
       </Alert>
     );
   }
-  return <Button onClick={() => setShow(true)}>Show Alert</Button>;
+  return <Button variant="warning" onClick={() => setShow(true)}>Latest Updates, CLick to Know !</Button>;
 }
 
 
