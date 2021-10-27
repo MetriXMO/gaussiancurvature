d159d474180c7d348beeb84f2541bcec4f7ead92
Aritra-12 <67796196+Aritra-12@users.noreply.github.com>
2021-10-28 01:44:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import Button from 'react-bootstrap/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAdjust } from '@fortawesome/free-solid-svg-icons'
 import './components/styles.css'
+import Particles from "react-particles-js";
+
 
 function App() {
 
@@ -65,5 +67,50 @@ function App() {
     </Router>
   );
 }
-
+export default function App2() {
+  return (
+    <Particles
+      params={{
+        background: {
+          color: {
+            value: "#000"
+          }
+        },
+        particles: {
+          number: {
+            value: 160,
+            density: {
+              enable: false
+            }
+          },
+          size: {
+            value: 10,
+            random: true
+          },
+          move: {
+            direction: "bottom",
+            outMode: "out"
+          },
+          links: {
+            enable: false
+          }
+        },
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "remove"
+            }
+          },
+          modes: {
+            remove: {
+              quantity: 10
+            }
+          }
+        }
+      }}
+    />
+  );
+}
 export default App;
+export default App2
