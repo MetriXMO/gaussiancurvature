16e5728bbab9c5c9074525602fd2878c957dd46d
Aritra-12 <67796196+Aritra-12@users.noreply.github.com>
2021-10-28 20:40:37 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,7 +28,7 @@ const Home = ({ darkMode }) => {
 
 <div align="center"  >  <Col lg={10}> 
 <h6 class="lead" align="left">
-    <br> </br>
+  
                  MetriX Mathematical Olympiad or  MMO as the name indicates is a mathematical olympiad which inherits the difficulty near to International Mathematical Olympiad or IMO. The exam is supposed to be held online mode every year and absolutely free for all so that everyone could enjoy the competition and explore the Olympiad flavour.
 To its  exception unlike IMO, this exam does not have any pre stages so as to attend this contest and is an single stage competition. This contest can be participated by participants from any country. However this contest is given by respective individuals and not teams and MMO 2021 is going to be held soon. </h6> 
            
