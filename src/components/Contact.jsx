0f6f5bd44a8dd012a3d9ad474dfaf464cac4e685
Aritra-12 <67796196+Aritra-12@users.noreply.github.com>
2021-10-29 02:40:46 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -48,7 +48,9 @@ const Contact = ({ darkMode }) => {
                     <Row>
       
 <br></br>
-Feel free to contact us anytime however don't contact for non formal things anytime. Our gmail is gaussiancurv180@gmail.com.
+<div className=lead">
+<h6>Feel free to contact us anytime however don't contact for non formal things anytime. Our gmail is gaussiancurv180@gmail.com. </h6>
+</div>
                 <br></br>
                         <Col lg={12}>
                             <br></br>
