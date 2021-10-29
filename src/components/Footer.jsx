d75c3b5c24fd9763d554432e9c26b49498e42500
Aritra-12 <67796196+Aritra-12@users.noreply.github.com>
2021-10-30 01:45:13 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <div>
             <hr/>
-            <p className="footer-text"><img src="https://imgur.com/WTq4Wfd.png" alt="hello" className="hello-img" width="145" />
+            <p className="footer-text"><img src="https://imgur.com/slwsIwe.png" alt="hello" className="hello-img" width="145" />
                 <br></br>
                 <small> <strong> Copyright © 2021 MMO </strong></small><br></br>
                
