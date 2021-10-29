57b823e18b85c8c2cfcf1a8ab37fd9629e560660
Aritra-12 <67796196+Aritra-12@users.noreply.github.com>
2021-10-30 01:21:34 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import helloLight from '../src/oeye.png';
+import helloDark from '../src/oeye.png';
 
 const Footer = () => {
     return (
         <div>
             <hr/>
-            <p className="footer-text"><h6>MMO</h6><br></br>
+            <p className="footer-text"><img src={darkMode ? helloDark : helloLight} alt="hello" className="hello-img" width="145" /><br></br>
                 <br></br>
-                <small> <h6> Copyright © 2021 All rights reserved by MMO. </h6></small><br></br>
+                <small> <strong> Copyright © 2021 All rights reserved by MMO. </strong><br></br>
                
             </p>
         </div>
