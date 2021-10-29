abdcb528aa90a6a6434e44aa9d4f829ffb41852a
Aritra-12 <67796196+Aritra-12@users.noreply.github.com>
2021-10-30 01:50:14 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <div>
             <hr/>
-            <p className="footer-text"> <h6>MMO<h6>
+            <p className="footer-text"> <h6>MMO</h6>
                 <br></br><br></br>
             <b> Copyright &copy; 2021 MMO </b><br></br>
                
