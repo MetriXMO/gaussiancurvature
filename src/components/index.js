a5749f6981bd69d7ed668694379ffb9bc6e35ebe
Aritra-12 <67796196+Aritra-12@users.noreply.github.com>
2021-10-30 11:31:47 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Experience } from './Experience';
 export { default as About } from './About';
 export { default as Contact } from './Contact';
 export { default as Footer } from './Footer';
+export { default as Team } from './Team';
