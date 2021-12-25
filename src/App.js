730ae53368a99f5a2d59af461ef46e917756e0d5
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2021-12-25 11:37:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -159,7 +159,7 @@ function App() {
                     <Nav.Link href="https://metrixmath-oly.vercel.app/contact" className="link"><h5>Contact</h5></Nav.Link>
                 </Nav>
                 <Button onClick={toggleDarkMode} variant={darkMode ? "dark" : "dark"}>
-                    {darkMode ? "Switch Dark Mode" : "Switch Light Mode"} <FontAwesomeIcon className="dark" size="lg" icon={faAdjust} spin/>
+                    {darkMode ? "Switch Dark Mode" : "Switch Light Mode"} <FontAwesomeIcon className="light" size="lg" icon={faAdjust} spin/>
                 </Button>
             </Navbar.Collapse>
           </Navbar>
