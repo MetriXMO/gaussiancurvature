34acd277f399907d316976373220a4b7717e88b3
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2021-12-26 20:07:43 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ const Home = ({ darkMode }) => {
       <Alert variant={darkMode ? "light" : "warning"} onClose={() => setShow(false)} dismissible>
         <Alert.Heading>Announcement</Alert.Heading>
         <p>
-          MMO registration has been started, for aops users the aops post is  <a href="https://artofproblemsolving.com/community/c5h2708626p23535219">here</a> and for non aops users you can submit your solutions through our gmail. The contest will start on 14th December, 2021 and end on 21st December. This is our <a href="https://discord.gg/vxmpsSSzuz">Discord Server</a>.
+        Here is  our <a href="https://discord.gg/vxmpsSSzuz">Discord Server</a>.
         </p>
       </Alert>
     );
