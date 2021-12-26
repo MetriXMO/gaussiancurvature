bd578894706d7a66ba038939cec5b9df35b3c189
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2021-12-26 20:15:27 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <hr/>
             <p className="footer-text"> <h6>MMO</h6>
                 <br></br><br></br>
-            <b> Copyright &copy; 2021 MMO </b><br></br>
+             Copyright &copy; 2021 MMO <br></br><br></br><br></br>
                
             </p>
         </div>
