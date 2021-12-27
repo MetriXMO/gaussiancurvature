efacca015a13340c75e27b614ba0e44e916ec8cb
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2021-12-27 18:09:52 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,6 @@ import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import { AttentionSeeker } from 'react-awesome-reveal';
-import { MagicalButton, Flex } from './stylee';
 
 const Home = ({ darkMode }) => {
  function AlertDismissibleExample() {
@@ -72,18 +71,7 @@ We also provide mathematical handouts to give some precise details about importa
                                    
                                     <Card.Text align="left" class={darkMode? "text-secondary" : "text-light"}>We are a group of Mathematical Olympiad students who admire Mathematics to a great extent. We wish to spread the joy of solving and recognising the non-routine level of mathematics and encourage all math lovers who are yet to discover or already discovered it to do so too. We also help sections of society that try to learn mathematics who have a great thirst for knowledge for mathematics and education.
 <br></br>
-<br></br>   <Flex>
-        <MagicalButton
-          href="https://docs.google.com/document/d/1yxCORtBMNxj_ukgOxoYQfRu3HTUqjtNXwcMajzcftF8"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          View resume
-        </MagicalButton>
-        <MagicalButton>
-          Get In Touch
-        </MagicalButton>
-      </Flex>
+<br></br>   
 <AlertDismissibleExample /> &nbsp; </Card.Text>
                                 </Card.Body></Card>
             </Col>
