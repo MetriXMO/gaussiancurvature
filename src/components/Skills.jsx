850c2244332b3acf35776691cb60bd41deaffc61
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2021-12-30 23:52:09 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -44,10 +44,10 @@ const Skills = ({ darkMode }) => {
                                 <Card.Body>
                                                <Card.Img variant="top" src={darkMode ? helloDark : helloLight} width="450"/> <br/><br/>
                                     <Card.Text align="left">
-                                                      MetriX Mathematical Olympiad held on 2020 Day 1 paper <br/>
-  <a href="https://aritra-12.github.io/pdfs/MMO%202020%20DAY%201.pdf" target="_blank" rel="noreferrer noopener"> <br/>
-                                            <MagicalButton  align="left">Download</MagicalButton> 
-                                        </a>&nbsp;</Card.Text>
+                                                      MetriX Mathematical Olympiad held on 2020 Day 1 paper <br/></Card.Text>
+ <Card.Header>  <a  target="_blank" rel="noreferrer noopener"> 
+                                            <MagicalButton href="https://aritra-12.github.io/pdfs/MMO%202020%20DAY%201.pdf"  align="left">Download</MagicalButton> 
+                                        </a>&nbsp; <Card.Header>
                                 </Card.Body>
                             </Card></Col>
  <Col lg={6}>
