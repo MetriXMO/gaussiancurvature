a7ad4af985b07190f41de8331e07c9db7b3f9ef7
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-01-01 19:54:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     <Switch>
         <Route exact path="/" >
           <Navbar expand="lg" className="navbar" data-theme={darkMode ? "dark" : "light"}>
-            <Navbar.Brand href="#home" className="navname"></Navbar.Brand>
+            <Navbar.Brand href="#home"  className="navname"></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
