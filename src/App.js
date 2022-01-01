14d0f3d6a077ec5f62afe994f55931f655fcf5c3
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-01-01 23:05:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,8 +39,8 @@ function App() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                  <Nav.Link href="https://gcurvature.vercel.app/" className="link">{darkMode? <MagicalButton>  <Dropdown>
-  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  <Nav.Link href="https://gcurvature.vercel.app/" className="link">{darkMode? <MagicalButton>  
+  <Dropdown.Toggle  id="dropdown-basic">
   Home
   </Dropdown.Toggle>
 
@@ -49,7 +49,7 @@ function App() {
     <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
     <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
   </Dropdown.Menu>
-</Dropdown> </MagicalButton> : <Mdark>Home</Mdark> }</Nav.Link>
+</MagicalButton> : <Mdark>Home</Mdark> }</Nav.Link>
                     <Nav.Link href="https://gcurvature.vercel.app/about" className="link">{darkMode? <MagicalButton>About Us</MagicalButton> : <Mdark>About Us</Mdark> }</Nav.Link>
                     <Nav.Link href="https://gcurvature.vercel.app/team" className="link">{darkMode? <MagicalButton>Team</MagicalButton> : <Mdark>Team</Mdark> }</Nav.Link>
                     <Nav.Link href="https://gcurvature.vercel.app/mmo2020" className="link">{darkMode? <MagicalButton>Contests</MagicalButton> : <Mdark>Contests</Mdark> }</Nav.Link>
